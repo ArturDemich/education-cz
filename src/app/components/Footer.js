bb2125adef662ecdbcd30a72b0fc8452c737c01e
ArturDemich <strelokar@gmail.com>
2024-02-02 00:55:33 +0200

    start project

---
+++ src/app/components/Footer.js
@@ -0,0 +1,12 @@
+
+
+const Footer = () => {
+
+    return (
+        <footer>
+            <h1>Education - CZ</h1>
+        </footer>
+    )
+}
+
+export default Footer
---
+++ src/app/components/Header.js
@@ -0,0 +1,19 @@
+
+
+const Header = () => {
+
+    return (
+        <header>
+            <div>
+                <div>Logo</div>
+                <div><h1>Education - CZ</h1></div>
+            </div>
+            <div>
+                <button>Contact with as</button>
+            </div>
+            
+        </header>
+    )
+}
+
+export default Header
